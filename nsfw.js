8c15215ad65b95e0de65cb28b91c1e74d9051f66
Emmy <emmybaylove@gmail.com>
2017-12-03 23:31:59 -0500

    Update nsfw.js
    more specific classes to try and avoid clashing with any pages that style the standard h1 and p elements

--- nsfw.js
+++ nsfw.js
@@ -11,30 +11,38 @@ nsfw.innerHTML =`
     width:100%;
     height:100%;
     position:fixed;
-    font-style:italic;
     top:0;
     left:0;
     background-color:#111;
     color:#fff;
     z-index:6969;
   }
-  #nsfw_overlay h1{
+  #nsfw_overlay div.h1{
+    width:100%;
+    display:block;
     margin:20% 0 0;
     font-size:1.75em;
     text-align:center;
     font-weight:normal;
   }
-  #nsfw_overlay p{
+  #nsfw_overlay div.p{
+    width:100%;
+    display:block;
     text-align:center;
-    font-size:1.15em;
+    font-size:1.1em;
     margin:.5em 0 0;
+    font-weight:lighter;
   }
-  #nsfw_overlay a{
+  #nsfw_overlay span.a{
+    cursor:pointer;
     color:#f55;
   }
+  #nsfw_overlay span.a:hover{
+    text-decoration:underline;
+  }
 </style>
-<h1>This page may contain explicit content.</h1>
-<p>If you understand and consent to viewing such content, <a href="#" onclick="closeOverlay()">click here to continue</a>.</p>
+<div class="h1">This page may contain explicit content.</div>
+<div class="p">If you understand and consent to viewing such content, <span class="a" onclick="closeOverlay()">click here to continue</span>.</div>
 `;
 var closeOverlay = () =>{ document.getElementById('nsfw_overlay').outerHTML = ''; },
     showOverlay = () =>{ document.body.appendChild(nsfw); };
