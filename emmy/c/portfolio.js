ba4c475ae92288f2bf4855496b8d0340f84dd8ac
Emmy <emmybaylove@gmail.com>
2017-12-08 01:43:45 -0500

    Update /emmy

---
+++ emmy/c/portfolio.js
@@ -0,0 +1,25 @@
+var scrollElem,
+adjustHead = () => {
+  var h = document.querySelector('#h.big');
+  h.style.height = `${window.innerHeight - 1}px`;
+},
+doParallax = () => {
+  scrollElem[0].style.backgroundPosition = `center calc(100% - ${window.scrollY * .2}px)`;
+  scrollElem[1].style.backgroundPosition = `center calc(100% - ${window.scrollY * .2}px)`;
+},
+initParallax = () => {
+  if (document.querySelector('#h:not(.no_banner)')) {
+    scrollElem = document.querySelectorAll('#h_bg, #h_separator');
+    window.addEventListener('scroll', doParallax);
+  }
+},
+// * * *
+pageLoad = () => {
+  if (document.querySelector('#h.big')) {
+    adjustHead();
+    window.addEventListener('resize', adjustHead);
+  }
+  initParallax();
+};
+// * * *
+window.addEventListener('load', pageLoad);
