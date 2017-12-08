5900ad0adbc6731b4930613fe25dbf7aee58f490
Emmy <emmybaylove@gmail.com>
2017-12-08 01:43:54 -0500

    Update nsfw.js

--- nsfw.js
+++ nsfw.js
@@ -1,10 +1,9 @@
 // Generates a page-blocking NSFW warning
 // Insert or embed (src="/nsfw.js") this script somewhere in the BODY of your page
 const nsfw = document.createElement('DIV');
-nsfw.setAttribute('id','nsfw_overlay');
 nsfw.innerHTML = `<style>body{overflow:hidden!important;}div#nsfw_overlay{width:100%;height:100%;position:fixed;top:0;left:0;background-color:#111;color:#fff;z-index:6969;}#nsfw_overlay div.nsfw_h1{width:100%;display:block;margin:20% 0 0;font-size:1.75em;text-align:center;font-weight:normal;}#nsfw_overlay div.nsfw_p{width:100%;display:block;text-align:center;font-size:1.1em;margin:.5em 0 0;font-weight:lighter;}#nsfw_overlay span.nsfw_a{cursor:pointer;color:#f55;}#nsfw_overlay span.nsfw_a:hover{text-decoration:underline;}</style>
 <div class="nsfw_h1">This page may contain explicit content.</div>
 <div class="nsfw_p">If you understand and consent to viewing such content, <span class="nsfw_a" onclick="closeOverlay()">click here to continue</span>.</div>`;
 var showOverlay = () =>{ document.body.appendChild(nsfw); },
-    closeOverlay = () =>{ document.getElementById('nsfw_overlay').outerHTML = ''; };
-showOverlay();
+    closeOverlay = () =>{ nsfw.outerHTML = ''; };
+window.addEventListener('load', showOverlay);
