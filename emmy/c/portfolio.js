e91dbfa81e71a2836ad3056e3a3d5c86286881fb
Emmy <emmybaylove@gmail.com>
2017-12-19 08:32:33 -0500

    Update /emmy

--- emmy/c/portfolio.js
+++ emmy/c/portfolio.js
@@ -10,6 +10,25 @@ initParallax = () => {
   scrollElem = document.querySelectorAll('#h_bg, #h_separator');
   window.addEventListener('scroll', doParallax);
 },
+insFooter = () => {
+  var footer = document.createElement('div'),
+      artists = document.head.getAttribute('data-art').split(', '),
+      artCredit = '',
+      artFunc = () => {
+        let artNum = artists.length / 2;
+        for (i = 0; i < artNum; i++) {
+          artCredit += `, <a href="${artists[(i * 2) + 1]}">${artists[i * 2]}</a>`;
+        }
+        artCredit = artCredit.substr(2);
+      };
+  artFunc();
+  footer.setAttribute('id', 'f');
+  footer.innerHTML =`
+    Artwork: ${artCredit}<br />
+    Website &copy; Emmy
+  `;
+  document.body.appendChild(footer);
+}
 // * * *
 pageLoad = () => {
   if (document.querySelector('#h.big')) {
@@ -19,6 +38,7 @@ pageLoad = () => {
   if (!document.querySelector('#h.no_banner')) {
     initParallax();
   }
+  insFooter();
 };
 // * * *
 window.addEventListener('DOMContentLoaded', pageLoad);
