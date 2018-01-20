670163e74f4878df2020ba294b29b67dc04532cf
Emmy <emmybaylove@gmail.com>
2018-01-20 02:52:15 -0500

    Update /emmy

--- emmy/theme/eso/eso.js
+++ emmy/theme/eso/eso.js
@@ -1,4 +1,4 @@
 var esoThemeScroll = () => {
-  document.body.style.backgroundPositionY = ((window.scrollY / 30) + 'px')
+  document.body.style.backgroundPositionY = `calc(100% - ${window.scrollY / 30}px)`;
 }
 document.body.addEventListener('scroll', esoThemeScroll);
