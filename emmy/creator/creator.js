27af5cdecb7f920d25993c9408772ea08bd39d2f
Emmy <emmybaylove@gmail.com>
2018-01-22 03:20:04 -0500

    Start work on profile creator

---
+++ emmy/creator/creator.js
@@ -0,0 +1,18 @@
+var artistList,
+theme = 'default',
+charName,
+playerName,
+headImgUrl,
+headImgSize,
+tagline,
+doc = {
+  head:`<head data-art="${artistList}" data-theme="${theme}"><title>${charName} &ndash; @${playerName}</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><link rel="stylesheet" type="text/css" href="/emmy/c/portfolio.css"><style>#h_bg,#h_separator{background-image:url('${headImgUrl}');}</style></head>`,
+  h:`<body><script src="/emmy/c/portfolio.js"></script><div id="h" class="${headImgSize}"><div id="h_bg"></div><div id="h_separator"></div><div id="h_title"><h1>${charName}</h1><h2>${tagline}</h2></div></div>`,
+  body:'',
+  init:() => {
+    var fullDoc = `<!DOCTYPE html><html lang="en">${this.head}${this.h}${this.body}</body></html>`;
+  }
+},
+modules = {
+
+}
